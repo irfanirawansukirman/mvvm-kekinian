1d10a023e6b53379ec0903e8ca646b5bec4a5def
irfanirawansukirman <dadang.kotz@gmail.com>
2019-07-27 11:03:32 +0700

    Fix: dummy movies list module, movie detail module, extras lib

--- app/src/main/java/id/pamoyanan_dev/khinantisticker/androidmvvmmystarter/MainActivity.kt
+++ app/src/main/java/id/pamoyanan_dev/khinantisticker/androidmvvmmystarter/MainActivity.kt
@@ -1,7 +1,7 @@
 package id.pamoyanan_dev.khinantisticker.androidmvvmmystarter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ f_movies_list/src/main/java/id/pamoyanan_dev/khinantisticker/f_movies_list/MoviesListActivity.kt
@@ -0,0 +1,12 @@
+package id.pamoyanan_dev.khinantisticker.f_movies_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.movies_list_activity)
+    }
+}
