33b5a1840b04739ac1002d9737bcd23612934365
irfanirawansukirman <dadang.kotz@gmail.com>
2019-08-02 13:15:36 +0700

    Fix: add recyclerview library

---
+++ lib_recyclerviewgenericadapter/build/generated/source/buildConfig/debug/com/leodroidcoder/genericadapter/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.leodroidcoder.genericadapter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.leodroidcoder.genericadapter";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.leodroidcoder.genericadapter";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000001;
+  public static final String VERSION_NAME = "0.0.1";
+}
